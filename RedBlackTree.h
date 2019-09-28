57a8a7474ead47afa565788473cd8a6558029103
Jakub <kub9818@gmail.com>
2019-09-28 23:47:07 +0200

    node adding method implementation started

--- RedBlackTree.h
+++ RedBlackTree.h
@@ -1,13 +1,10 @@
 #ifndef RBTREE_REDBLACKTREE_H
 #define RBTREE_REDBLACKTREE_H
 
-//#define BLACK 1
-//#define RED 0
-
 #include <utility>
 #include <memory>
-#include <bits/unique_ptr.h>
-#include <bits/shared_ptr.h>
+//#include <bits/unique_ptr.h>
+//#include <bits/shared_ptr.h>
 //#include "RBNode.h"
 
 namespace tree_map
@@ -18,53 +15,63 @@ namespace tree_map
     public:
         using valueType = std::pair<keyType, mappedType>;
         //enum color_t { RED = 0, BLACK };
-        // using unsigned char = color; //its wrong, look on the enum
     private:
         /*
          * Definition of nested class representing node of tree, hidden implementation needed
          */
         enum color_t { RED = 0, BLACK };
-        struct RBNode //struct(?)
+        struct RBNode
         {
             valueType nodeData;
-            std::unique_ptr<RBNode> rightChild;
-            std::unique_ptr<RBNode> leftChild;
+            std::shared_ptr<RBNode> rightChild; // unique ptrs changed to shared ptrs due to recursive algorithms
+            std::shared_ptr<RBNode> leftChild;
             std::shared_ptr<RBNode> parent; // changed raw pointers to smart pointers
             color_t nodeColor; //added enum
 
-            RBNode( valueType dataPair = std::make_pair( keyType(), mappedType() ), RBNode* rightC = nullptr, RBNode* leftC = nullptr, RBNode* pParent = nullptr )
+            explicit RBNode( valueType dataPair = std::make_pair( keyType(), mappedType() ), RBNode* rightC = nullptr, RBNode* leftC = nullptr, RBNode* pParent = nullptr )
                 : nodeData(dataPair), rightChild(rightC), leftChild(leftC), parent(pParent), nodeColor(RED)
-            {} // def ctor with no args
-
-//            RBNode( keyType key = keyType(), mappedType mapped = mappedType(), RBNode* rightC = nullptr, RBNode* leftC = nullptr, RBNode* pParent = nullptr )
-//                : nodeData(std::make_pair(key, mapped)), rightChild(rightC), leftChild(leftC), parent(pParent), nodeColor(RED)
-//            {} // ctor with no args so maybe ambiguous and should be deleted(?)
-
-//            RBNode( valueType dataPair, RBNode* rightC = nullptr, RBNode* leftC = nullptr, RBNode* pParent = nullptr )
-//                : nodeData(dataPair), rightChild(rightC), leftChild(leftC), parent(pParent), nodeColor(RED)
-//            {} //ctor
-//
-//            RBNode( keyType key, mappedType mapped, RBNode* rightC = nullptr, RBNode* leftC = nullptr, RBNode* pParent = nullptr )
-//                : nodeData(std::make_pair(key, mapped)), rightChild(rightC), leftChild(leftC), parent(pParent), nodeColor(RED)
-//            {} //ctor with making pair from key and mapped value
+            {} // def ctor
         }; // RBNode
 
-        std::unique_ptr<RBNode> root;
-        std::unique_ptr<RBNode> sentinel;
+        std::shared_ptr<RBNode> root;
+        std::shared_ptr<RBNode> sentinel;
         unsigned int treeSize;
 //      class test;
 //      RBTreeIterator<keyType, mappedType> iterator;
 //      RBTreeConstIterator<keyType, mappedType> constIterator;
+        void addNode( const keyType& key, const mappedType& mapped = mappedType() /*const valueType& node_data = std::make_pair( keyType(), mappedType() )*/ )
+        {
+            auto nodeValue = std::make_pair(key, mapped);
+            std::shared_ptr<RBNode> nodePtr( new RBNode(nodeValue) );
+
+            if(!root) // if tree's empty
+            {
+                nodePtr->leftChild = sentinel;
+                nodePtr->rightChild = sentinel;
+                nodePtr->nodeColor = BLACK;
+                root = nodePtr;
+                ++treeSize;
+            }
+        }
     public:
         RedBlackTree()
             : root(nullptr), sentinel( new RBNode() ), treeSize(0)
         {
-            sentinel -> rightChild = std::move(sentinel);
-            //sentinel -> leftChild = std::move(sentinel);
-            if( sentinel -> parent.get() == nullptr )
-                std::cout << "dupa";
+//          sentinel -> rightChild = std::move(sentinel);
+//          sentinel -> leftChild = std::move(sentinel);
+//            if( sentinel -> parent == nullptr )
+//                std::cout << "test";
             sentinel -> nodeColor = BLACK;
         }
+
+        ~RedBlackTree() { treeSize = 0; } // set tree empty, smart pointers take care of root and sentinel TODO: recursive call deleteTree()
+
+        void test( const keyType& k, const mappedType& m = mappedType() )
+        {
+            addNode(k,m); // works for adding first node
+        }
+
+        //void addNode
     };
 }
 
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     //std::cout << "Hello, World!" << std::endl;
-    tree_map::RedBlackTree<int, char> testClass;
-
+    tree_map::RedBlackTree<int, char> testObj;
+    testObj.test(20);
     return 0;
 }
